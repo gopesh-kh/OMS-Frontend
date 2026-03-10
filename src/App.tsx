afd38b1d44cdad5f8e57362ad8963c56036ef2f5
= <=>
2026-03-10 09:49:15 +0530

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
+import Auth from "./pages/Auth";
+import Product from "./pages/Product";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/auth" element={<Auth />}>
+        <Route path="login" />
+        <Route path="signup" />
+      </Route>
+      <Route path="/products" element={<Home />}>
+        <Route path=":productId" element={<Product />} />
+      </Route>
+      <Route path="/cart" element={<Cart />}></Route>
+      <Route path="/account" element={<Account />}>
+        <Route index path="viewprofile" />
+        <Route path="editprofile" />
+        <Route path="orders">
+          <Route path=":orderId" />
+        </Route>
+      </Route>
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
     <>
       <NavBar />
+      <Outlet />
+      <Footer />
     </>
   );
 };
